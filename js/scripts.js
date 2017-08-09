38d05d5adf5418b11d62b2a0fea71dbdcf1b7101
Noah Gottlieb and Riki Montgomery <noah+riki@>
2017-08-09 11:59:51 -0700

    Remove redundant id tags and clean up the user letter.

--- js/scripts.js
+++ js/scripts.js
@@ -10,16 +10,16 @@ $(document).ready(function() {
     $(".finalLetter").text(letterBody);
 
 
-    // $("letter").show();
+    $("#letterStyle").show();
 
     event.preventDefault();
   });
 
     $("#letter").submit(function() {
-      // $("#letterStyle").hide();
       $("letterStyle").slideToggle(800);
       $("#letter").slideToggle(800);
-      $("h1").slideToggle(800)
+      $(".extraText").slideToggle(800);
+      $("h1").slideToggle(800);
     });
 
 });
