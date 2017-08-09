73c96f0222492e4204d96f9b4044c5175b606460
Noah Gottlieb and Riki Montgomery <riki@>
2017-08-09 10:18:49 -0700

    make letter form

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,17 @@
 $(document).ready(function() {
-  $
+  $("#letter").submit(function() {
+    var firstName = $("input#firstName").val();
+    var lastName = $("input#lastName").val();
+    var letterBody= $("input#letterBody").val();
+
+
+    $(".firstName").text(firstName);
+    $(".lastName").text(lastName);
+    $(".finalLetter").text(letterBody);
+
+
+    $("letter").show();
+
+    event.preventDefault();
+  });
+});
