b43898978b9af1843687a9bbfafe8e35d01c1af9
Noah Gottlieb and Riki Montgomery <noah+riki@>
2017-08-09 11:28:22 -0700

    Add css styles to the letter.

--- js/scripts.js
+++ js/scripts.js
@@ -10,8 +10,16 @@ $(document).ready(function() {
     $(".finalLetter").text(letterBody);
 
 
-    $("letter").show();
+    // $("letter").show();
 
     event.preventDefault();
   });
+
+    $("#letter").submit(function() {
+      // $("#letterStyle").hide();
+      $("letterStyle").slideToggle(800);
+      $("#letter").slideToggle(800);
+      $("h1").slideToggle(800)
+    });
+
 });
